83e552cdc73e87dee41151186347ea38e04b3792
Paulo Duque <paulo.duque@terra.com.br>
2022-07-28 20:11:58 -0300

    fullture-flix_desafio_2807

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -12,22 +12,24 @@ export function Cards(){
     },[])
 
     return(
-        <div>
-            {!filmes ? "loading" :   // Se não houver conteúdo em "filme" escreve loading na tela. Se houver, entra no MAP
-            <>                               
-                {filmes.map((filme)=>{
-                   return (
-                    <Container key={filme.id}>
-                        <img
-                            src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`}
-                            alt={filme.title}
-                        />
-                        <h4>{filme.title}</h4>    
-                        <h4>{filme.vote_average}</h4> 
-                   </Container> 
-                   );  
-                })}
-            </>}
-        </div>
+        <Grid>
+            <div>
+                {!filmes ? "loading" :   // Se não houver conteúdo em "filme" escreve loading na tela. Se houver, entra no MAP
+                <>                               
+                    {filmes.map((filme)=>{
+                    return (
+                        <Container key={filme.id}>
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`}
+                                alt={filme.title}
+                            />
+                            <h4>{filme.title}</h4>    
+                            <h4>{filme.vote_average}</h4> 
+                        </Container> 
+                    );  
+                    })}
+                </>}
+            </div>
+        </Grid>    
     );
 }
