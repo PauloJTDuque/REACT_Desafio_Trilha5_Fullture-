d5cffee3d4b1ba8469a9bbbffaea4822989d3517
Paulo Duque <paulo.duque@terra.com.br>
2022-08-04 19:14:12 -0300

    fullture-flix_desafio_0408

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,12 +1,26 @@
 import { Footer } from "../../components/footer/Footer";
 import { Header } from "../../components/header/Header";
+import { Grid, MainContainer, RigthSide } from "./DetailsStyle";
+import {Link} from 'react-router-dom'
 
-export default function Details(){
-    return(
-        <div>
-            <Header/>
-            <main> Div do Meio</main>
-            <Footer/>
-        </div>
-    )
-}
+export default function Details() {
+    return (
+      <MainContainer>
+        <Header />
+        <Grid>
+          {/* <img src={`https://image.tmdb.org/t/p/w500/${state.data.poster_path}`} alt="" onClick={()=>{console.log(state)}}/>
+            <RigthSide>
+              <p><b>Titulo:</b> {state.data.title}</p>
+              <p><b>Sinopse:</b> {state.data.overview}</p>
+              <p><b>Data de lançamento:</b> {state.data.release_date}</p>
+              <p><b>Nota:</b> {state.data.vote_average}</p>
+              <Link to="/" onClick={()=>dispatch(goToIndex())}>Voltar para a home</Link>
+            </RigthSide> */}
+            <Link to="/">
+              <button>Voltar para home</button>
+            </Link>
+        </Grid>
+        <Footer />
+      </MainContainer>
+    );
+  }
